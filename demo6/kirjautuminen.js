9e35a2abfbf726f24339b8342d9d82da15ccfad7
matiaskeuda <159139464+matiaskeuda@users.noreply.github.com>
2024-02-26 00:50:23 +0200

    Add files via upload

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",onkoKirjautunut);
+
+function onkoKirjautunut() 
+{
+let kirjautunut = 'kyllä';
+
+if(kirjautunut === 'kyllä') {
+
+document.getElementById('tervetulo_teksti').textContent += ' matias';
+
+
+}
+
+}
+
